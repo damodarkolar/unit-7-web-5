2581d70000b28f8ae4af47d118ae3924b284957b
damodarkolar <damodarkolar@gmail.com>
2022-09-19 10:06:03 +0530

    add

--- 2-http/benchMarking/index.js
+++ 2-http/benchMarking/index.js
@@ -5,9 +5,25 @@ const PATH="./text.txt";
 
 
 const server=http.createServer((req, res)=>{
-console.log(req.url)
-let data=fs.readFile(PATH, {encoding:"UTF-8"})
-res.end(JSON.stringify(data))
+
+    let PATH="./textLarge.txt"
+    let urlMsg=req.url;
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`<div style="width:50%; display:flex; flex-direction:column; justify-content:centre; align-item:centre;margin-left:500px"><h1>Different Types of File read System</h1><ul><li><a href="/sync"> Read File in Sync</a></li><li><a href="/nonSync"> Read File in ASync</a></li></ul></div>`)
+    if(urlMsg=="/sync"){
+        let data=fs.readFileSync(PATH, {encoding:"UTF-8"});
+        res.write(`<h3>${data}</h3>`)
+        res.end("<h1>aaa</h1>ended");
+    }else if(urlMsg=="/nonSync"){
+        let data=fs.readFile(PATH, {encoding:"UTF-8"},(err, data)=>{
+            if(err){
+                res.write("something wend wrong")
+            }else{
+                res.write(`<h3>${data}</h3>`)
+            }
+        }); 
+    }
+    
 })
 
 
