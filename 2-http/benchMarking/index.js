cece89986bbc4fb3596707d834cbb704e7a76c9c
damodarkolar <damodarkolar@gmail.com>
2022-09-19 20:19:15 +0530

    first ev added

--- 2-http/benchMarking/index.js
+++ 2-http/benchMarking/index.js
@@ -9,21 +9,24 @@ const server=http.createServer((req, res)=>{
     let PATH="./textLarge.txt"
     let urlMsg=req.url;
     res.setHeader('Content-Type', 'text/html');
-    res.write(`<div style="width:50%; display:flex; flex-direction:column; justify-content:centre; align-item:centre;margin-left:500px"><h1>Different Types of File read System</h1><ul><li><a href="/sync"> Read File in Sync</a></li><li><a href="/nonSync"> Read File in ASync</a></li></ul></div>`)
-    if(urlMsg=="/sync"){
+    res.write(`<div style="width:50%; display:flex; flex-direction:column; justify-content:centre; align-item:centre;margin-left:500px"><h1>Different Types of File read System</h1><ul><li><a href="/textsync"> Read File in Sync</a></li><li><a href="/textasync"> Read File in ASync</a></li><li><a href="/textstream"> Read File in Stream</a></li></ul></div>`)
+    if(urlMsg=="/textsync"){
         let data=fs.readFileSync(PATH, {encoding:"UTF-8"});
         res.write(`<h3>${data}</h3>`)
         res.end("<h1>aaa</h1>ended");
-    }else if(urlMsg=="/nonSync"){
-        let data=fs.readFile(PATH, {encoding:"UTF-8"},(err, data)=>{
+    }else if(urlMsg=="/textasync"){
+        fs.readFile(PATH, {encoding:"UTF-8"},(err, data)=>{
             if(err){
                 res.write("something wend wrong")
             }else{
                 res.write(`<h3>${data}</h3>`)
             }
         }); 
+    }else if(urlMsg=="/textstream"){
+        const stream = fs.createReadStream(`${__dirname}/data.txt`);
+        stream.pipe(res);
     }
-    
+    // res.end()   
 })
 
 
--- evaluations/first-evaluation/index.js
+++ evaluations/first-evaluation/index.js
@@ -1,1 +1,34 @@
-console.log("hello world!")
+const express=require("express");
+const fs=require("fs");
+const PORT=8080;
+
+const app=express();
+app.use(express.json());
+
+app.get("/getmeip",(req, res)=>{
+res.send("ip")
+});
+
+app.get("/products",(req, res)=>{
+    res.send("get")
+});
+
+app.post("/products",(req, res)=>{
+    res.send("POST")
+});
+
+app.delete("/product/:id",(req, res)=>{
+    res.send("delete")
+});
+
+app.patch("/product/:id",(req, res)=>{
+    res.send("Patch")
+});
+
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`server is started in port ${PORT}`)
+})
